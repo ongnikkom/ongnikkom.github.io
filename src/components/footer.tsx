1799cc85cec403c1a17cde60d2eee6a2caffd137
Nikko Ong <zniik.0ng@gmail.com>
2024-08-15 15:23:40 +0400

    initial commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,59 @@
+import { Separator } from "@/shadcn/ui/separator";
+import {
+  FaFacebookSquare,
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,
+} from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <footer className="bg-white border-t flex py-6 dark:bg-black dark:border-t-[#272727]">
+      <div className="container flex flex-col">
+        <div className="text-center md:self-end md:text-left">
+          <h3 className="font-bold">Contact</h3>
+          <p className="opacity-80 mt-2 text-sm">
+            If you’d like to get in touch, feel free to reach out via{" "}
+            <a className="underline" href="mailto:ong.nikko.m@gmail.com">
+              email
+            </a>
+          </p>
+        </div>
+
+        <Separator className="my-6" />
+
+        <div className="flex flex-col justify-between sm:flex-row sm:items-center">
+          <p className="opacity-80 text-center text-sm">
+            &copy; {new Date().getFullYear()} Nikko Ong. All rights reserved.
+          </p>
+
+          <ul className="flex gap-4 justify-center mt-4 md:mt-0">
+            <li>
+              <a href="https://github.com/ongnikkom">
+                <FaGithub className="size-6" />
+              </a>
+            </li>
+
+            <li>
+              <a href="https://www.linkedin.com/in/ongnikko">
+                <FaLinkedin className="size-6" />
+              </a>
+            </li>
+
+            <li>
+              <a href="https://instagram.com/ongnikko.m">
+                <FaInstagram className="size-6" />
+              </a>
+            </li>
+
+            <li>
+              <a href="https://www.facebook.com/ongnikko.m">
+                <FaFacebookSquare className="size-6" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,28 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/shadcn/ui/avatar";
+import { ThemeToggle } from "./theme-toggle";
+
+export default function Header() {
+  return (
+    <header className="bg-white border-b flex h-[75px] items-center dark:bg-black dark:border-b-[#272727]">
+      <div className="container flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Avatar className="border-2 dark:border-[#272727] size-12">
+            <AvatarImage src="/assets/img/pp.jpg" alt="@ongnikkom" />
+            <AvatarFallback>NO</AvatarFallback>
+          </Avatar>
+          <div>
+            <a href="/">
+              <h1 className="font-bold">Nikko Ong</h1>
+            </a>
+
+            <p className="opacity-80 text-xs">
+              Fullstack Developer | Guitar Enthusiast | Tech Lover
+            </p>
+          </div>
+        </div>
+
+        <ThemeToggle />
+      </div>
+    </header>
+  );
+}
